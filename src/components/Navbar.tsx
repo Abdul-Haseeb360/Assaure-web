6dbc6f0cdc29383d7d772910068edc6165983a36
Abdul-Haseeb360 <aheducation786@gmail.com>
2025-03-19 15:51:36 +0500

    fix:image-issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import hero from "../../Public/herosection.jpg";
-import Logo from "../../public/assurelogo.png";
 import { navigation } from "@/constant/data";
 import HeroSection from "./HeroSection";
 import { Container } from "./Container";
@@ -11,43 +9,51 @@ export default function Hero() {
     <div className="relative h-[711px]">
       {/* Background Image */}
       <Container>
-      <div className="absolute inset-0">
-        <div className="absolute inset-0 bg-[#06250FD4] " />
-        <Image
-          src={hero}
-          alt=""
-          priority
-          className="self-stretch  z-10 w-full h-full"
-        />
-      </div>
+        <div className="absolute inset-0">
+          <div className="absolute inset-0 bg-[#06250FD4] " />
+          <Image
+            src="/herosection.jpg"
+            alt=""
+            priority
+            className="self-stretch  z-10 w-full h-full"
+            width={1440}
+            height={711}
+          />
+        </div>
 
-      <div className="relative w-full ">
-        <header className="absolute top-4  left-0 right-0 z-10 px-4 py-4 md:py-6 bg-white rounded-lg flex flex-col md:flex-row justify-between items-center">
-          <div className="flex ">
-            <Image src={Logo} alt="ASSURE logo icon" className="mr-6" />
-          </div>
-          <div className="hidden gap-8 lg:flex">
-            {navigation.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className="text-base font-medium text-gray-700 hover:text-gray-900"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
+        <div className="relative w-full ">
+          <header className="absolute top-4  left-0 right-0 z-10 px-4 py-4 md:py-6 bg-white rounded-lg flex flex-col md:flex-row justify-between items-center">
+            <div className="flex ">
+              <Image
+                src="/assurelogo.png"
+                alt="ASSURE logo icon"
+                className="mr-6"
+                width={150}
+                height={40}
+              />
+            </div>
+            <div className="hidden gap-8 lg:flex">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className="text-base font-medium text-gray-700 hover:text-gray-900"
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
 
-          <Link
-            href="/contact"
-            className="rounded-lg bg-[#F58220] px-6 py-3 text-base font-medium text-white hover:bg-[#F58220]/90"
-          >
-            Contact now
-          </Link>
-        </header>
-        <HeroSection />
-      </div>
-    </Container>
+            <Link
+              href="/contact"
+              className="rounded-lg bg-[#F58220] px-6 py-3 text-base font-medium text-white hover:bg-[#F58220]/90"
+            >
+              Contact now
+            </Link>
+          </header>
+          <HeroSection />
+        </div>
+      </Container>
     </div>
   );
 }
